fb7985ad1548e2f1aa301910eb8587b626146a49
GameAndDev <gameanddev22@gmail.com>
2016-07-20 20:15:03 +0200

    ajout de la recuperation du canvas et des erreur

--- src.js
+++ src.js
@@ -3,16 +3,17 @@ window.onload = function()
     var canvas = document.getElementById('mon_canvas');
         if(!canvas)
         {
-            alert("Impossible de récupérer le canvas");
+            console.log("Impossible de récupérer le canvas");
             return;
         }
 
     var context = canvas.getContext('2d');
         if(!context)
         {
-            alert("Impossible de récupérer le context du canvas");
+            console.log("Impossible de récupérer le context du canvas");
             return;
         }
+        console.log("récupération du canvas réussie");
 
 
     //C'est ici que l'on placera tout le code servant à nos dessins.
