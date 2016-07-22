cdd6c3434b66783715a74bffef5751879d634e23
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-07-22 19:37:35 +0200

    indentation du code et utilisation de canvas.height

--- src.js
+++ src.js
@@ -1,32 +1,51 @@
 window.onload = function()
 {
-    var canvas = document.getElementById('mon_canvas');
-        if(!canvas)
+    var canvas = document.getElementById("myCanvas");
+    if(!canvas)
+    {
+        alert("Impossible de récupérer le canvas");
+        return;
+    }
+
+    var context = canvas.getContext("2d");
+    if(!context)
+    {
+        alert("Impossible de récupérer le context");
+        return;
+    }
+
+    var diametreBalle = 5;
+
+    var posX = 1+diametreBalle/2;
+    var posY = 1+diametreBalle/2;
+    var vitesseX = 3;
+    var vitesseY = 3;
+
+    var myInterval = setInterval(animate, 1000/30);
+
+    function animate()
+    {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        //Tracé de la balle
+        context.beginPath();
+        context.arc(posX, posY, diametreBalle/2, 0, Math.PI*2);
+        context.fill();
+
+        //On va vérifier si la balle à toucher l'un des bords du canvas.
+        if(posX+diametreBalle/2 >= canvas.width || posX <= 0+diametreBalle/2)//Si on touche le bord gauche ou droit
         {
-            console.log("Impossible de récupérer le canvas");
-            return;
+            vitesseX *= -1;//On inverse la vitesse de déplacement sur l'axe horizontal.
         }
 
-    var context = canvas.getContext('2d');
-        if(!context)
+        if(posY+diametreBalle/2 >= canvas.height || posY <= 0+diametreBalle/2)//Si on touche le bord du bas ou du haut
         {
-            console.log("Impossible de récupérer le context du canvas");
-            return;
+            vitesseY *= -1;//On inverse la vitesse de déplacement sur l'axe vertical.
         }
-        console.log("récupération du canvas réussie");
-var Xscreen = window.innerWidth;
-var Yscreen = window.innerHeight;
-console.log("Xscreen = " + Xscreen );
-console.log("Yscreen = " + Yscreen);
-var Xcircle = Xscreen/2;
-var Ycircle = Yscreen/2;
 
-context.beginPath();
-context.fillStyle = "#e91c1c";
-context.arc(Xcircle, Ycircle, 50, 0, Math.PI*2);
-context.fill();
-context.closePath();
+        //On additionne les vitesses de déplacement avec les positions
+        posX += vitesseX;
+        posY += vitesseY;
+    }
 
-
-    //C'est ici que l'on placera tout le code servant à nos dessins.
 }
